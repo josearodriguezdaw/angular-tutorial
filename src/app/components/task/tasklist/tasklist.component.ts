b548940109ce540a1ec8299e532e74a64286d50f
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-11-11 18:02:20 +0100

    Ej3: Funcionalidad componente TaskList

--- src/app/components/task/tasklist/tasklist.component.ts
+++ src/app/components/task/tasklist/tasklist.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task, TaskPriority, TaskStatus } from '../../../models/task.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tasklist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tasklist.component.html',
   styleUrl: './tasklist.component.css'
 })
-export class TasklistComponent {
+export class TasklistComponent implements OnInit{
+  listaTareas:Task[] = [];
+  
+  ngOnInit(): void {
+    let task1:Task = new Task (1,"Tarea 1", "Descripción Tarea 1",TaskPriority.Baja,TaskStatus.Pendiente,new Date("11/1/2024"),new Date("11/18/2024"),false);
+    let task2:Task = new Task (2,"Tarea 2", "Descripción Tarea 2",TaskPriority.Alta,TaskStatus.EnProgreso,new Date("11/5/2024"),new Date("11/16/2024"),false);
+    let task3:Task = new Task (3,"Tarea 3", "Descripción Tarea 3",TaskPriority.Baja,TaskStatus.EnProgreso,new Date("11/21/2024"),new Date("11/30/2024"),false);
+    let task4:Task = new Task (4,"Tarea 4", "Descripción Tarea 4",TaskPriority.Alta,TaskStatus.Completada,new Date("11/8/2024"),new Date("11/21/2024"),false);
+    let task5:Task = new Task (5,"Tarea 5", "Descripción Tarea 5",TaskPriority.Media,TaskStatus.Pendiente,new Date("11/10/2024"),new Date("11/30/2024"),false);
+    this.listaTareas = [task1,task2,task3,task4,task5];
+  }
 
 }
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,52 @@
+export enum TaskStatus{
+    Completada="C",
+    EnProgreso="EP",
+    Pendiente="P"
+}
+
+export enum TaskPriority{
+    Alta="A",
+    Media="M",
+    Baja="B"
+}
+
+export class Task {
+    id:number;
+    nombre:string;
+    descripcion:string;
+    prioridad:TaskPriority;
+    estado:TaskStatus;
+    fechaCreacion:Date;
+    fechaExpiracion:Date;
+    isDelete:boolean;
+
+    constructor (id:number,nombre:string,descripcion:string,prioridad:TaskPriority,estado:TaskStatus,fechaCreacion:Date,fechaExpiracion:Date,isDelete:boolean){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion=descripcion;
+        this.prioridad=prioridad;
+        this.estado = estado;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaExpiracion =fechaExpiracion;
+        this.isDelete = isDelete;
+    }
+
+    getTextoEstado():string{
+        let text="";
+        switch(this.estado){
+            case "EP": text="En proceso";break;
+            case "C": text="Realizada"; break;
+            case "P": text="Pendiente";break;
+        }
+        return text;
+    }
+
+    getTextoPrioridad():string{
+        switch(this.prioridad){
+            case "A": return "Alta";break;
+            case "M": return "Media"; break;
+            case "B": return "Baja";break;
+            default: return "";
+        }
+    }
+}
