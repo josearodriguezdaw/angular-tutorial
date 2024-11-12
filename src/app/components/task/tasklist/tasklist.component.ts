69848da1fcbe77fa2b2770083663e2d19dfbbd42
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-11-12 20:27:10 +0100

    Ej2: adaptado código fuente a Libro Blanco

--- src/app/components/task/tasklist/tasklist.component.ts
+++ src/app/components/task/tasklist/tasklist.component.ts
@@ -10,15 +10,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './tasklist.component.css'
 })
 export class TasklistComponent implements OnInit{
-  listaTareas:Task[] = [];
+  taskList:Task[] = [];
   
   ngOnInit(): void {
-    let task1:Task = new Task (1,"Tarea 1", "Descripción Tarea 1",TaskPriority.Baja,TaskStatus.Pendiente,new Date("11/1/2024"),new Date("11/18/2024"),false);
-    let task2:Task = new Task (2,"Tarea 2", "Descripción Tarea 2",TaskPriority.Alta,TaskStatus.EnProceso,new Date("11/5/2024"),new Date("11/16/2024"),false);
-    let task3:Task = new Task (3,"Tarea 3", "Descripción Tarea 3",TaskPriority.Baja,TaskStatus.EnProceso,new Date("11/21/2024"),new Date("11/30/2024"),false);
-    let task4:Task = new Task (4,"Tarea 4", "Descripción Tarea 4",TaskPriority.Alta,TaskStatus.Completada,new Date("11/8/2024"),new Date("11/21/2024"),false);
-    let task5:Task = new Task (5,"Tarea 5", "Descripción Tarea 5",TaskPriority.Media,TaskStatus.Pendiente,new Date("11/10/2024"),new Date("11/30/2024"),false);
-    this.listaTareas = [task1,task2,task3,task4,task5];
+    let task1:Task = new Task (1,"Tarea 1", "Descripción Tarea 1",TaskPriority.LOW,TaskStatus.PENDING,new Date("11/1/2024"),new Date("11/18/2024"),false);
+    let task2:Task = new Task (2,"Tarea 2", "Descripción Tarea 2",TaskPriority.HIGH,TaskStatus.IN_PROGRESS,new Date("11/5/2024"),new Date("11/16/2024"),false);
+    let task3:Task = new Task (3,"Tarea 3", "Descripción Tarea 3",TaskPriority.LOW,TaskStatus.IN_PROGRESS,new Date("11/21/2024"),new Date("11/30/2024"),false);
+    let task4:Task = new Task (4,"Tarea 4", "Descripción Tarea 4",TaskPriority.HIGH,TaskStatus.COMPLETED,new Date("11/8/2024"),new Date("11/21/2024"),false);
+    let task5:Task = new Task (5,"Tarea 5", "Descripción Tarea 5",TaskPriority.MEDIUM,TaskStatus.PENDING,new Date("11/10/2024"),new Date("11/30/2024"),false);
+    this.taskList = [task1,task2,task3,task4,task5];
   }
 
 }
--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,51 +1,51 @@
 export enum TaskStatus{
-    Completada="C",
-    EnProceso="EP",
-    Pendiente="P"
+    COMPLETED="C",
+    IN_PROGRESS="IP",
+    PENDING="P"
 }
 
 export enum TaskPriority{
-    Alta="A",
-    Media="M",
-    Baja="B"
+    HIGH="H",
+    MEDIUM="M",
+    LOW="L"
 }
 
 export class Task {
     id:number;
-    nombre:string;
-    descripcion:string;
-    prioridad:TaskPriority;
-    estado:TaskStatus;
-    fechaCreacion:Date;
-    fechaExpiracion:Date;
+    name:string;
+    description:string;
+    priority:TaskPriority;
+    status:TaskStatus;
+    creationDate:Date;
+    expirationDate:Date;
     isDelete:boolean;
 
-    constructor (id:number,nombre:string,descripcion:string,prioridad:TaskPriority,estado:TaskStatus,fechaCreacion:Date,fechaExpiracion:Date,isDelete:boolean){
+    constructor (id:number,name:string,description:string,priority:TaskPriority,status:TaskStatus,creationDate:Date,expirationDate:Date,isDelete:boolean){
         this.id = id;
-        this.nombre = nombre;
-        this.descripcion=descripcion;
-        this.prioridad=prioridad;
-        this.estado = estado;
-        this.fechaCreacion = fechaCreacion;
-        this.fechaExpiracion =fechaExpiracion;
+        this.name = name;
+        this.description=description;
+        this.priority=priority;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.expirationDate =expirationDate;
         this.isDelete = isDelete;
     }
 
-    getTextoEstado():string{
+    getStatusText():string{
         let text="";
-        switch(this.estado){
-            case "EP": text="En proceso";break;
+        switch(this.status){
+            case "IP": text="En proceso";break;
             case "C": text="Realizada"; break;
             case "P": text="Pendiente";break;
         }
         return text;
     }
 
-    getTextoPrioridad():string{
-        switch(this.prioridad){
-            case "A": return "Alta";break;
+    getPriorityText():string{
+        switch(this.priority){
+            case "H": return "Alta";break;
             case "M": return "Media"; break;
-            case "B": return "Baja";break;
+            case "L": return "Baja";break;
             default: return "";
         }
     }
